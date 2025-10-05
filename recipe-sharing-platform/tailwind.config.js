fcbf43ffe516d265b8ad318f6d4af80544f49af2
moses ogbeche <mosesogbeche@gmail.com>
2025-10-05 13:52:20 +0100

    Fix Tailwind CSS configuration and inclusion setup

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -2,10 +2,11 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
